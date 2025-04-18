043b209a264da4d2e3bd4ea01a226d4fb7d0c6f6
Watchik Grigorjan <watchik101@gmail.com>
2025-04-18 10:47:31 +0200

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,15 +1,30 @@
-prijzen = {"aardbei": 3, "vanille": 4, "chocolade": 5}
-aanbieding = prijzen["aardbei"] * 0.8
-reclame_tekst = f"Vandaag in de aanbieding: aardbeienijs, 1 liter – slechts € {aanbieding}"
-reclame_tekst2 = reclame_tekst[:64]
-reclame_tekst3 = reclame_tekst2.lower()
-reclame_tekst4 = reclame_tekst3.split()
+from helper import decoreer
 
-for el in reclame_tekst4:
-    if len(el) >= 5:
-        print(el.upper())
-    else:
-        print(el)
+def print_aanbieding():
+    prijzen = {
+        "aardbei": 3,
+        "vanille": 4,
+        "chocolade": 5
+    }
+
+    aanbieding = prijzen["aardbei"] * 0.8
+    
+    reclame_tekst = f"Vandaag in de aanbieding: aardbeienijs, 1 liter – slechts € {aanbieding}"
+    
+    reclame_tekst2 = reclame_tekst[:63]
+    
+    reclame_tekst3 = reclame_tekst2.upper()
+    
+    reclame_tekst4 = reclame_tekst3.split()
+
+    for el in reclame_tekst4:
+        if len(el) > 4:
+            print(el.upper())
+        else:
+            print(el.lower())
+
+decoreer("Aanbieding")
+print_aanbieding()
 # Dictionaries in Python: Je moet de dictionary definiëren met accolades {} en de sleutel-waarde paren scheiden met dubbele punten :. Dus {"aardbei": 3, "vanille": 4, "chocolade": 5} in plaats van losse variabelen.
 # Toegang tot Dictionary Elementen: Om de waarde van "aardbei" te krijgen, gebruik je prijzen["aardbei"].
 # Berekening van de Aanbiedingsprijs: Je moet de vermenigvuldiging uitvoeren op de waarde uit de dictionary, niet op een string.
